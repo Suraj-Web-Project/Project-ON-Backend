929b17ef54e9df3b48835bafb06b081a74db49c0
Suraj-code-project <surajnayak1304@gmail.com>
2025-08-14 00:11:53 +0530

    add user and video model with token

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./.env" }); // ensure it loads your .env
 
-(async () => {
-  try {
-    await connectDB();
-    // Start your server here if needed
-  } catch (error) {
-    console.error("ERROR:", error.message);
-  }
-})();
+
+connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  });
+})
+.catch((error) => {
+  console.log("Mongo db connection failed !!!", error);
+  
+
+})
